f283946a4daecc290a033e16c9e1d75899d8247f
jimenabarrerau <145827740+jimenabarrerau@users.noreply.github.com>
2023-11-15 19:33:45 -0600

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -11,4 +11,18 @@ st.title("Título: Analítica de datos")
 st.header("Este es un header")
 st.subheader("Este es un subheader")
 
-st.write('Hello world! Jimena :sunglasses:')
+st.text("Texto: Hola streamlit")
+st.markdown("#Este es un markdown h1 \n ## Este es un h2  \n ### Este es un h3")
+st.header("Colores de texto y mensajes de error")
+st.success("Successful")
+st.info("Información!")
+st.warning("warning")
+st.error("Error")
+st.exception("NameError('no está definido')")
+st.header("Obtener información de ayuda de Python")
+st.help(range)
+st.header("Widgets:")
+st.subheader("Checkbox
+             
+"""#Checkbox
+#st.write('Hello world! Jimena :sunglasses:')
