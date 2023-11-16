d84d0e3043c291b192c60705a1dc218a80b67b23
jimenabarrerau <145827740+jimenabarrerau@users.noreply.github.com>
2023-11-15 19:25:38 -0600

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,2 +1,14 @@
 import streamlit as st
+import datetime
+import time
+import pandas as pd
+import streamlit as st
+import PIL import image
+
+"""Generación de la webapp con streamlit"""
+
+st.title("Título: Analítica de datos")
+st.header("Este es un header")
+st.subheader("Este es un subheader")
+
 st.write('Hello world! Jimena :sunglasses:')
