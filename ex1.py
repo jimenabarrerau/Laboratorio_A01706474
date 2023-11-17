02b866d77cce9d16a7c0b12d094a0e081c43efbf
jimenabarrerau <145827740+jimenabarrerau@users.noreply.github.com>
2023-11-16 18:36:24 -0600

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,27 +1,56 @@
-import streamlit as st
-import datetime
+import datetime 
 import time
 import pandas as pd
 import streamlit as st
+from PIL import Image
 
 """Generación de la webapp con streamlit"""
-
-st.title("Título: Analítica de datos")
-st.header("Este es un header")
+# Definir titulo
+st.title("Titulo: Analitica de Datos")
+#Definir Header/SubHeader
+st.header('Este es un header')
 st.subheader("Este es un subheader")
-
-st.text("Texto: Hola streamlit")
-st.markdown("#Este es un markdown h1 \n ## Este es un h2  \n ### Este es un h3")
+#Definir en texto
+st.text("Texto: Hola Streamlit")
+#Definición de MarkDown
+st.markdown("Este es un markdown h1 \n ## Este es un h2 \n ### Este es un h3")
 st.header("Colores de texto y mensajes de error")
 st.success("Successful")
 st.info("Información!")
 st.warning("warning")
 st.error("Error")
-st.exception("NameError('no está definido')")
+st.exception("NameError(no está definido)")
 st.header("Obtener información de ayuda de Python")
 st.help(range)
 st.header("Widgets:")
 st.subheader("Checkbox")
-             
-"""#Checkbox
-#st.write('Hello world! Jimena :sunglasses:')
+
+if st.checkbox("Show/Hide"):
+    st.text("Mostrar u ocultar Widget")
+    st.subheader("Radio buttons")
+
+status = st.radio("Cual es su estatus", ("Activo", "Inactivo"))
+
+if status == "Activo":
+    st.success("Estás activo")
+else: 
+    st.warning("Inactivo")
+    st.subheader("SelectBox")
+
+occupation = st.selectbox(
+    "Tu ocupación", ["Programador", "Científico de datos", "BI", "Ingeniero de Datos"]
+)
+
+st.write("Opción seleccionada:", occupation)
+st.subheader("MultiSelect")
+
+location = st.multiselect(
+    "Donde trabajas?",
+    ("México", "New York", "Guadalajara", "Monterrey", "Nepal", "Buenos Aires", "Caracas"),
+)
+
+st.write("Seleccionó:", len(location), "locaciones")
+st.subheader("Slider")
+
+level = st.slider("Cual es tu nivel?", 1, 5)
+st.write("Nivel:", level)
